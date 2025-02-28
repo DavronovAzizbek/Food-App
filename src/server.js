11b9c7fa98139e1fb0b5d3cfce8dd78ad4f86557
githubusername <azizbekdavronov1110@gmail.com>
2025-03-01 01:51:56 +0500

    The necessary packages for the project have been installed and configured.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-  return res.status(200).send("<h1>Welcome to Food Server APP </h1>");
+  return res
+    .status(200)
+    .send("<h1>Welcome to Food Server APP API BASE PROJECT </h1>");
 });
 
-const PORT = 7000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log("Server Running");
+  console.log(`Server running on ${PORT}`.white.bgMagenta);
 });
