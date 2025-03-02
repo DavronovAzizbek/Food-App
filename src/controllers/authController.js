a467f4fc4eb452605e6a60336cb5376bc4118118
githubusername <azizbekdavronov1110@gmail.com>
2025-03-02 23:53:02 +0500

    The login part is also ready

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -42,6 +42,39 @@ const registerController = async (req, res) => {
   }
 };
 
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide Email or Password",
+      });
+    }
+
+    const user = await userModel.findOne({ email: email, password: password });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "User Not Found OR Password MisMatch",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Login Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Login API",
+      error,
+    });
+  }
+};
+
 module.exports = {
   registerController,
+  loginController,
 };
