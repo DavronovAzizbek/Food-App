8b9c26ea5dcbf9ce3cf95a80af7106bd8f8190a3
githubusername <azizbekdavronov1110@gmail.com>
2025-03-03 23:35:23 +0500

    foodSchema and createFood

---
+++ src/controllers/foodController.js
@@ -0,0 +1,56 @@
+const foodModal = require("../models/foodModal");
+
+const createFoodController = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      price,
+      imageUrl,
+      foodTags,
+      category,
+      code,
+      isAvailable,
+      restaurant,
+      rating,
+    } = req.body;
+
+    if (!title || !description || !price || !restaurant) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide All Fields",
+      });
+    }
+
+    const newFood = new foodModal({
+      title,
+      description,
+      price,
+      imageUrl,
+      foodTags,
+      category,
+      code,
+      isAvailable,
+      restaurant,
+      rating,
+    });
+
+    await newFood.save();
+    res.status(201).send({
+      success: true,
+      message: "New Food Item Created",
+      newFood,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Create Food API",
+      error,
+    });
+  }
+};
+
+module.exports = {
+  createFoodController,
+};
