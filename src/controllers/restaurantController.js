a34b97b6e32ebedcf2efb5060e23cc7cb6eae47b
githubusername <azizbekdavronov1110@gmail.com>
2025-03-03 16:20:43 +0500

    createRestaurant added

---
+++ src/controllers/restaurantController.js
@@ -0,0 +1,60 @@
+const restaurantModel = require("../models/restaurantModel");
+
+const createRestaurantController = async (req, res) => {
+  try {
+    const {
+      title,
+      imageUrl,
+      foods,
+      time,
+      pickup,
+      delivery,
+      isOpen,
+      logoUrl,
+      rating,
+      ratingCount,
+      code,
+      coords,
+    } = req.body;
+
+    if (!title || !coords) {
+      return res.status(500).send({
+        success: false,
+        message: "Please Provide Title and Address",
+      });
+    }
+
+    const newRestaurant = new restaurantModel({
+      title,
+      imageUrl,
+      foods,
+      time,
+      pickup,
+      delivery,
+      isOpen,
+      logoUrl,
+      rating,
+      ratingCount,
+      code,
+      coords,
+    });
+
+    await newRestaurant.save();
+
+    res.status(201).send({
+      success: true,
+      message: "New Restaurant Created Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Create Restaurant API",
+      error,
+    });
+  }
+};
+
+module.exports = {
+  createRestaurantController,
+};
